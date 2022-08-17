c2924e0c52a7f1eda68fa73bbe9450a65035a6a5
rabeahmed2002 <89692415+rabeahmed2002@users.noreply.github.com>
2022-08-17 10:59:35 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,24 +4,18 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-900 to-black text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-900 to-black text-white "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-5xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-5xl font-bold text-center">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
-        Junior MERN stack developer and a passionate Ethereum Blockchain developer with a demonstrated history of working in the computer software industry. Skilled in Solidity, Javascript, React.js, Node.js, Python, C++, Microsoft Excel, and Graphic Design with a Bachelor of Science - BS focused in Computer Science from Karachi University.
-          
-        </p>
-
-        <br />
-
-        <p className="text-xl">
-        Junior MERN stack developer and a passionate Ethereum Blockchain developer with a demonstrated history of working in the computer software industry. Skilled in Solidity, Javascript, React.js, Node.js, Python, C++, Microsoft Excel, and Graphic Design with a Bachelor of Science - BS focused in Computer Science from Karachi University.
+        <p className="text-xl text-center">
+        A junior web developer focused on MERN stack development, having an experience of 3 months in building React.js apps. I am currently in 2nd year of my Bachelors of Computer Science from the University of Karachi, UBIT.
+        I have worked on a range of technologies which demonstrates my ability adapt to work environment and my quick learning ability. I am always motivated to design and develop websites and solve complex problems.
         </p>
       </div>
     </div>

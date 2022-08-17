5aed5076f3e8909a5d0f585ec9e0dba9ab5241d5
rabeahmed2002 <89692415+rabeahmed2002@users.noreply.github.com>
2022-08-17 10:58:42 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 
   return (
     <div className='nav'>
-      <div className='flex justify-between items-center w-full h-20 text-white fixed bg-black px-4'>
+      <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4'>
         <div>
         <h1 className='text-5xl font-signature ml-2'>Rabe</h1>
         </div>
